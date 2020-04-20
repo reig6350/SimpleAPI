afb8a6487ec36609ecad14073820a334cb329b9e
Shaun Reiger <reigers@mymacewan.ca>
2020-04-20 10:44:28 -0600

    Removed https redirection

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -36,8 +36,7 @@ namespace SimpleAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
+           
             app.UseRouting();
 
             app.UseAuthorization();
