650e01dd1046b67ee6b14b12bea5a2b02d47aa1a
Shaun Reiger <reigers@mymacewan.ca>
2020-04-20 22:08:42 -0600

    Removed ILogger variable

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -16,7 +16,7 @@ namespace SimpleAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        //private readonly ILogger<WeatherForecastController> _logger;
 
 /*
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
